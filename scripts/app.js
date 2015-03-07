e091cb29e758e7d81c1df582a479ec029ba377f1
Aldin Fajic <afajic@hotmail.com>
2015-03-07 16:09:06 -0500

    App files

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular
+  .module('devicesManagerApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    'ui.bootstrap'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
